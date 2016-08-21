ad3fecf8c20cf9abf513996703503d3f91d193f9
ricky0624 <ricky23750719@gmail.com>
2016-08-21 16:07:56 +0800

    2016-08-21

--- src/tw/org/iii/classroom/Class13.java
+++ src/tw/org/iii/classroom/Class13.java
@@ -6,33 +6,34 @@ public class Class13
 	{
 		int[] p=new int[6];
 		
-		for(int i=0;i<100;i++)
+		for(int i=0;i<10000;i++)
 		{
-			int point=(int)(Math.random()*9+1); //1-9的亂數
-			switch(point)
-			{
-			case 1:
-				p[0]++;
-				break;
-			case 2:
-				p[1]++;
-				break;
-			case 3:
-				p[2]++;
-				break;
-			case 4: case 7:
-				p[3]++;
-				break;
-			case 5: case 8:
-				p[4]++;
-				break;
-			case 6: case 9:
-				p[5]++;
-				break;
-			default:
-				System.out.println("Error");
-				break;
-			}
+			int point=(int)(Math.random()*9); //0-8的亂數
+			p[(point>=6?point-3:point)]++;
+//			switch(point)
+//			{
+//			case 1:
+//				p[0]++;
+//				break;
+//			case 2:
+//				p[1]++;
+//				break;
+//			case 3:
+//				p[2]++;
+//				break;
+//			case 4: case 7:
+//				p[3]++;
+//				break;
+//			case 5: case 8:
+//				p[4]++;
+//				break;
+//			case 6: case 9:
+//				p[5]++;
+//				break;
+//			default:
+//				System.out.println("Error");
+//				break;
+//			}
 		}
 		for(int i=0;i<p.length;i++)
 		{
